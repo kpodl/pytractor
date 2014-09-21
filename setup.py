fda17d18d941e78800b7556c93d78500cf74fdf6
Konrad Podloucky <kpodl@users.noreply.github.com>
2014-09-21 21:41:55 +0200

    Bumped version for PyPI release
    Fixed package url

--- setup.py
+++ setup.py
@@ -30,13 +30,13 @@ with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
 setup(
     name='pytractor',
 
-    version='0.0.1.dev1',
+    version='0.0.1',
 
     description='Selenium testing for Angular.js apps',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/kpodl/pytractor',
 
     # Author details
     author='Konrad Podloucky',
